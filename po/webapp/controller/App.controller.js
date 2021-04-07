129751a02e8ab154c2a67daf6de7fc98bcf77e9c
Logali-Group <training@logaligroup.com>
2021-04-07 20:22:45 +0000

    second commit

--- po/webapp/controller/App.controller.js
+++ po/webapp/controller/App.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 	"sap/ui/model/json/JSONModel"
 ], function (BaseController, JSONModel) {
 	"use strict";
-
+//comment
 	return BaseController.extend("ns.po.controller.App", {
 
 		onInit : function () {
